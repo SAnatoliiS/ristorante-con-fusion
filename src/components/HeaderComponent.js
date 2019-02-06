d3d9330eb676f94e7129e6aac88f492254154046
16758351 <sergeev.an.ser@sberbank.ru>
2019-02-06 17:00:19 +0300

    React Animations

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -55,7 +55,7 @@ class Header extends Component {
         <Navbar dark expand="md">
           <div className="container">
             <NavbarToggler onClick={this.toggleNav} />
-            <NavbarBrand className="mr-auto" href="/">
+            <NavbarBrand className="mr-auto" href="/home">
               <img
                 src="assets/images/logo.png"
                 height="30"
